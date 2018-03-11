fcc50deb356e49489b9282f7a8bf6c19b063f66e
NguyenSonArsenal <vanson297.nguyen@gmail.com>
2018-03-11 13:42:10 +0700

    + add

--- assets/js/index.js
+++ assets/js/index.js
@@ -4,5 +4,25 @@ $(document).ready(function(){
         infinite: true,
         autoplay: true,
         autoplaySpeed: 2000,
+        variableWidth:true,
+        responsive: [
+            {
+                breakpoint: 600,
+                settings: {
+                    slidesToShow: 3,
+                    slidesToScroll: 2
+                }
+            },
+            {
+                breakpoint: 480,
+                settings: {
+                    slidesToShow: 3,
+                    slidesToScroll: 1
+                }
+            }
+            // You can unslick at a given breakpoint now by adding:
+            // settings: "unslick"
+            // instead of a settings object
+        ]
     });
 });
